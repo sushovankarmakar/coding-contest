5326cda02524f6f10446e0b38288babd9ee6c83a
Sushovan <officialkarmakar@gmail.com>
2021-11-28 14:52:40 +0530

    biweekly 66 & weekly 269

--- leetcode/biweekly/biweekly66/_2085_CountCommonWordsWithOneOccurrence.java
+++ leetcode/biweekly/biweekly66/_2085_CountCommonWordsWithOneOccurrence.java
@@ -1,5 +1,40 @@
 package biweekly66;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/contest/biweekly-contest-66
+ * https://leetcode.com/problems/count-common-words-with-one-occurrence/
+ */
+
 public class _2085_CountCommonWordsWithOneOccurrence {
+
+  public int countWords(String[] words1, String[] words2) {
+
+    Map<String, Integer> wordFreq1 = new HashMap<>();
+    Map<String, Integer> wordFreq2 = new HashMap<>();
+
+    for (String word : words1) {
+      wordFreq1.put(word, wordFreq1.getOrDefault(word, 0) + 1);
+    }
+
+    for (String word : words2) {
+      wordFreq2.put(word, wordFreq2.getOrDefault(word, 0) + 1);
+    }
+
+    int count = 0;
+
+    for (String word : words1) {
+
+      if (wordFreq1.containsKey(word) && wordFreq2.containsKey(word)) {
+        if (wordFreq1.get(word) == 1 && wordFreq2.get(word) == 1) {
+          count++;
+        }
+      }
+    }
+
+    return count;
+  }
 
 }
--- leetcode/weekly/weekly269/_2091_RemovingMinAndMaxFromArray.java
+++ leetcode/weekly/weekly269/_2091_RemovingMinAndMaxFromArray.java
@@ -1,7 +1,47 @@
 package weekly269;
 
+/**
+ * https://leetcode.com/contest/weekly-contest-269/
+ * https://leetcode.com/problems/removing-minimum-and-maximum-from-array/
+ *
+ * https://leetcode.com/problems/removing-minimum-and-maximum-from-array/discuss/1599848/JavaC%2B%2BPython-Straight-Forward-Solutions
+ */
+
 public class _2091_RemovingMinAndMaxFromArray {
 
-  
+  public int minimumDeletions(int[] nums) {
 
+    int n = nums.length;
+
+    int min = nums[0], minIndex = 0;
+    int max = nums[0], maxIndex = 0;
+
+    for (int i = 1; i < n; i++) {
+      if (nums[i] > max) {
+        max = nums[i];
+        maxIndex = i;
+      } else if (nums[i] < min) {
+        min = nums[i];
+        minIndex = i;
+      }
+    }
+
+    /**
+     * There can only be three scenarios for deletions such that both minimum and maximum elements are removed:
+     *       Scenario 1: Both elements are removed by only deleting from the front / left.
+     *       Scenario 2: Both elements are removed by only deleting from the back / right.
+     *       Scenario 3: Delete from the front to remove one of the elements,
+     *                   and delete from the back to remove the other element.
+     *
+     *       Compare which of the three scenarios results in the minimum number of moves.
+     */
+
+    int deleteFromLeft = 1 + Math.max(minIndex, maxIndex); // Scenario 1
+
+    int deleteFromRight = n - Math.min(minIndex, maxIndex); // Scenario 2
+
+    int deleteFromBothSides = ((1 + Math.min(minIndex, maxIndex)) + (n - Math.min(minIndex, maxIndex)));  // Scenario 3
+
+    return Math.min(deleteFromBothSides, Math.min(deleteFromLeft, deleteFromRight));
+  }
 }
